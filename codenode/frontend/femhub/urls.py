160a47ad71b64ec5865d055763855705634061b5
Mateusz Paprocki <mattpap@gmail.com>
2010-06-08 12:41:28 +0200

    Fixed urls after switching jsonrpc

--- codenode/frontend/femhub/urls.py
+++ codenode/frontend/femhub/urls.py
@@ -7,6 +7,6 @@ from codenode.frontend.femhub.views import femhub
 urlpatterns = patterns('',
     url(r'^$', femhub, name='femhub'),
     url(r'^json/$', jsonrpc_site.dispatch, name='jsonrpc_mountpoint'),
-    url(r'^json/browse/$', 'jsonrpc.views.browse', name='jsonrpc_browser'),
+    url(r'^json/browse/$', 'codenode.external.jsonrpc.views.browse', name='jsonrpc_browser'),
 )
 
